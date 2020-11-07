9fce3dc90abf6fd2f589eebf578577366539029d
hopmatubbi <57031614+hopmatubbi@users.noreply.github.com>
2020-11-07 13:01:04 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,215 +1,271 @@
 ﻿using System;
-using System.Runtime.CompilerServices;
-using System.Security.Cryptography.X509Certificates;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
-namespace lab1
+namespace OOP_L3
 {
+    public partial class Customer
+    {
+        public string country { get; set; }
+        public readonly string company;
+        public int deadline;
+        static private int num;
+
+        //Конструктор по умолчанию (а)
+        public Customer()
+        {
+            country = "Germany";
+            company = "CiLOa";
+            deadline = 3;
+            Customer.Number();
+        }
+        
+        //Конструктор с параметрами (b)
+        public Customer(string n, string c, int d)
+        {
+            company = n;
+            country = c;
+            deadline = d;
+            Customer.Number();
+        }
+
+        //Статический конструктор (c)
+        static Customer()
+        {
+            Console.WriteLine("Ваши заказы:");
+        }
+
+        static class OutStatic
+        {
+            static public void OutStatic1(Customer cst)
+            {
+                Console.WriteLine(cst.ToString());
+            }
+        }
+
+        //Конструктор копирования (d)
+        public Customer(Customer previousCustomer)
+        {
+            company = previousCustomer.company;
+            country = previousCustomer.country;
+            deadline = previousCustomer.deadline;
+            Customer.Number();
+        }
+
+        //Деструктор (f)
+        ~Customer()
+        {
+            Console.WriteLine("Деструктор");
+        }
+
+        //Статический метод (g) 
+        static public int Number()
+        {
+            return ++num;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.OutputEncoding = System.Text.Encoding.UTF8;
-            Console.Write("Введите тип boolean: ");
-            bool tr = Convert.ToBoolean(Console.ReadLine());
-            Console.Write("Введите тип byte: ");
-            byte bt = Convert.ToByte(Console.ReadLine());
-            Console.Write("Введите тип sbyte: ");
-            sbyte sbt = Convert.ToSByte(Console.ReadLine());
-            //Console.Write("Введите тип char: ");
-            //char chr = Convert.ToChar(Console.ReadLine());
-            Console.Write("Введите тип decimal: ");
-            decimal dcml = Convert.ToDecimal(Console.ReadLine());
-            Console.Write("Введите тип double: ");
-            double dbl = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Введите тип float: ");
-            float flt = Convert.ToSingle(Console.ReadLine());
-            Console.Write("Введите тип Int32: ");
-            int a = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Введите тип uint: ");
-            uint nt = Convert.ToUInt32(Console.ReadLine());
-            Console.Write("Введите тип long: ");
-            long lng = Convert.ToInt64(Console.ReadLine());
-            Console.Write("Введите тип ulong: ");
-            ulong ulng = Convert.ToUInt64(Console.ReadLine());
-            Console.Write("Введите тип short: ");
-            short shrt = Convert.ToInt16(Console.ReadLine());
-            Console.Write("Введите тип ushort: ");
-            ushort ushrt = Convert.ToUInt16(Console.ReadLine());
-            Console.Write("Введите тип string: ");
-            string strng = Console.ReadLine();
-            Console.Clear();
-            Console.WriteLine("Тип boolean: " + tr);
-            Console.WriteLine("Тип byte: " + bt);
-            Console.WriteLine("Тип sbyte: " + sbt);
-            //Console.WriteLine("Тип char: " + chr);
-            Console.WriteLine("Тип decimal: " + dcml);
-            Console.WriteLine("Тип double: " + dbl);
-            Console.WriteLine("Тип float: " + flt);
-            Console.WriteLine("Тип Int32: " + a);
-            Console.WriteLine("Тип uint: " + nt);
-            Console.WriteLine("Тип long: " + lng);
-            Console.WriteLine("Тип ulong: " + ulng);
-            Console.WriteLine("Тип short: " + shrt);
-            Console.WriteLine("Тип ushort: " + ushrt);
-            Console.WriteLine("Тип string: " + strng);
-            Console.WriteLine("Введите 'y', если ходите продолжить: ");
-            char v = Convert.ToChar(Console.ReadLine());
-            if (v == 'y')
+            Customer customer1 = new Customer();
+            Console.WriteLine(customer1.ToString());
+
+            Customer customer2 = new Customer("BelAIrline", "Belarus", 2);
+            if (customer2.deadline <= 0 || customer2.company == "" || customer2.company == null)
             {
-                Console.Clear();
+                Console.WriteLine("Некорректные данные");
             }
             else
-                return;
-            //2zadanie
-            int x = 15, y=7;
-            dbl = (double)x/y;
-            bt = (byte)x;
-            sbt = (sbyte)y;
-            flt = (float)x/y;
-            lng = (long)x/y;
-            Console.WriteLine("Явное привидение:");
-            Console.WriteLine("Тип double: " + dbl);
-            Console.WriteLine("Тип byte: " + bt);
-            Console.WriteLine("Тип sbyte: " + sbt);
-            Console.WriteLine("Тип float: " + flt);
-            Console.WriteLine("Тип long: " + lng);
-            dbl = x / y;
-            flt = x / y;
-            lng = x / y;
-            Console.WriteLine("Неявное привидение:");
-            Console.WriteLine("Тип double: " + dbl);
-            Console.WriteLine("Тип float: " + flt);
-            Console.WriteLine("Тип long: " + lng);
-            Console.WriteLine("Введите 'y', если ходите продолжить: ");
-            v = Convert.ToChar(Console.ReadLine());
-            if (v == 'y')
             {
-                Console.Clear();
+                Console.WriteLine(customer2.ToString());
+            }
+
+            Customer customer3 = new Customer("ALMI", "Russia", 6);
+            Console.WriteLine(customer3.ToString());
+
+            Customer customer4 = new Customer(customer2);
+            Console.WriteLine(customer4.ToString());
+            Console.ReadLine();
+
+            if (customer1.Equals(customer2))
+            {
+                Console.WriteLine("Заказы равны");
             }
             else
-                return;
-            //3zadanie
-            Console.Write("Введите число: ");
-            int price = Convert.ToInt32(Console.ReadLine());
-            //boxing
-            Object obj = price;
-            Console.WriteLine("Boxing: " + obj);
-            //unboxing
-            int price2 = (int)obj;
-            Console.WriteLine("Unboxing: " + price2);
-            Console.WriteLine("Введите 'y', если ходите продолжить: ");
-            v = Convert.ToChar(Console.ReadLine());
-            if (v == 'y')
             {
-                Console.Clear();
+                Console.WriteLine("Заказы не равны");
+            }
+            Console.ReadLine(); 
+            Console.WriteLine($"Hashcode for customer1: {customer1.GetHashCode()}");
+            Console.WriteLine($"Hashcode for customer2: {customer2.GetHashCode()}");
+            Console.WriteLine($"Hashcode for customer3: {customer3.GetHashCode()}");
+            Console.WriteLine($"Hashcode for customer4: {customer4.GetHashCode()}");
+            Console.ReadLine();
+            
+            int x = 10;
+            int y = 15;
+            int z = 0;
+            Addition(ref x, y);
+            Console.WriteLine("Сумма x и y: " + x);
+
+            Sum(20, 40, out z);
+            Console.WriteLine("Разность x и y: " + z);
+            Console.ReadLine();
+
+            // Анонимный тип
+            var customer = new { company = "FITA", country = "Belgium", deadline = 10 };
+            Console.WriteLine(customer.ToString());
+            Console.WriteLine(customer.GetType());
+            Console.ReadLine();
+
+            MyCollection<Customer> customer5 = new MyCollection<Customer>();
+            customer5.addItem(customer1);
+            customer5.addItem(customer3);
+            customer5.printAll();
+            Customer customer6 = new Customer("Tweebykvai", "Belarus", 5);
+            if (customer5.attend(customer6))
+            {
+                Console.WriteLine("True");
             }
             else
-                return;
-            //4zadanie
-            dbl = x;//Так будет работать
-            Console.WriteLine("Переменная типа int стала типа double: " + dbl.GetType());
-           //
-            Console.WriteLine("Введите 'y', если ходите продолжить: ");
-            v = Convert.ToChar(Console.ReadLine());
-            if (v == 'y')
             {
-                Console.Clear();
+                Console.WriteLine("False");
             }
-            else
-                return;
-            //5zadanie
-            int? x1 = null;
-            int? x2 = null;
-            System.Console.Write(x1 == x2);
-            //6zadanie
-            var qq = 'a';
-            Console.WriteLine("Var: " + qq);
-            //qq = 51;//Т.к. вар уже стал типа char
-            //2ZADANIE
-            //Azadanie
-            string ftp1 = "HELLO MY NEIGHBOUR";
-            string ftp2 = "LIVING BELARUS";
-            Console.WriteLine(String.Compare(ftp2, ftp1));
-            //Bzadanie
-            string b= "1";
-            string c = "2";
-            string d = "3";
-            Console.WriteLine("Concat: " + String.Concat(d, b));
-            c = String.Copy(b);
-            Console.WriteLine("Copy: " + d);
-            string name1 = "LUCKY PERSON";
-            string name2 = name1.Substring(4);
-            Console.WriteLine("Substring: " + name2);
-            string text = "This is a text. New string";
-            char[] delims = ".,;:!?\n\xD\xA\" ".ToCharArray();
-            string[] words = text.Split(delims,
-            StringSplitOptions.RemoveEmptyEntries);
-            foreach (string word in words)
-                Console.WriteLine(word);
-            Console.WriteLine("Insert: "+ ftp2.Insert(5, ftp1));
-            Console.WriteLine("Remove: " + ftp2.Remove(5));
-            var person = new { Name = "Gary", Age = 43 };
-            Console.WriteLine($"Имя: {person.Name}  Возраст: {person.Age}");
-            //3zadanie
-            string s1 = "";
-            Console.WriteLine("Is null or empty? "+ String.IsNullOrEmpty(s1));
-            string s2 = null;
-            Console.WriteLine("Is null or empty? " + String.IsNullOrEmpty(s2));
-            Console.WriteLine("Concat with empty: " + String.Concat(s1, name1));
-            Console.WriteLine("Concat with null: " + String.Concat(s2, name1));
-            //4zadanie
-            StringBuilder v1 = new StringBuilder("ABC",50);
-            v1 = v1.Remove(1,2);
-            v1.Append("Idk");
-            v1.Insert(0,"FTP");
-            Console.WriteLine("{0} chars: {1}", v1.Length,v1.ToString());
-            //3ZADANIE
-            //Bzadanie
-            string[] arr = { "I", "love", "this", "world" };
-            foreach (string i in arr)
-            {
-                Console.Write(i + " ");
-            }
-            Console.Write(arr.Length);
-            //Dzadanie
-            var t3d1 = new[] { 1, 2, 3, 4, 5, 6 };
-            var t3d2 = "text";
-            Console.WriteLine();
-            Console.Write("Вывод неявно типизированного массива: ");
-            for (int i = 0; i < t3d1.Length; i++)
-                Console.Write("{0}\t", t3d1[i]);
-            Console.WriteLine();
-            Console.WriteLine("Вывод неявно типизированной строки: {0} ", t3d2);
-            //4zadanie
-            //Azadanie
-            (int first,string second,char third,string fourth,ulong fifth) namesAndAge = (19,"MIKITA",'m',"MINSK",2001);
-            //Bzadanie
-            Console.WriteLine("Age:"+namesAndAge.first+", Sex:" + namesAndAge.third+ ", City:" + namesAndAge.fourth);
-            //Czadanie
-            (int first, string second, char third, string fourth, ulong fifth) qwerty = (18, "Katya", 'f',"MOSCOW",2002);
-            qwerty.CompareTo(namesAndAge);
-            Console.WriteLine(qwerty.second);
-          
+            MyCollection<Customer> customer10 = new MyCollection<Customer>();
+            customer10.addItem(customer1);
+            customer10.addItem(customer6);
+            MyCollection<Customer> customer7 = customer5.Merge(customer10);
+            Console.WriteLine("customer7: ");
+            customer7.printAll();
+
+            Singleton<int> singleton = new Singleton<int>();
+            singleton.addItem(25);
+            singleton.addItem(15);
+
+            Console.WriteLine("\nSingleton : ");
+            singleton.printAll();
+            Singleton<int> singleton2 = new Singleton<int>();
+            Console.WriteLine("\nSingleton2 : ");
+            singleton2.printAll();
+            singleton.addItem(100);
+            Console.WriteLine("\nAfter add to first. The second: ");
+            singleton2.printAll();
+            Console.ReadLine();
+            Console.ReadLine();
         }
-        static int[] Sort(int[] Array, int i)
+        // параметр x передается по ссылке
+        static void Addition(ref int x, int y)
         {
-            int swtch=0;
-            for(int x=0;x<i; x++)
-            {
-                if (x + 1 == i) ;
-                else
-                {
-                    if (Array[x] > Array[x + 1])
-                    {
-                        swtch = Array[x];
-                        Array[x] = Array[x + 1];
-                        Array[x + 1] = swtch;
-                        x = 0;
-                    }
-                }
-            }
-            return Array;
+            x += y;
+        }
+
+        // выходной параметр z 
+        static void Sum(int x, int y, out int z)
+        {
+            z = x - y;
         }
     }
-}
+
+    public class MyCollection<T>
+    {
+        private T[] items = new T[0];
+        public int Count = 0;
+        public void addItem(T item)
+        {
+            Array.Resize(ref items, items.Count() + 1);
+            items[items.Count() - 1] = item;
+            Count++;
+        }
+        public void removeItem(T item)
+        {
+            items[Array.IndexOf(items, item)] = items[items.Count() - 1];
+            Array.Resize(ref items, items.Count() - 1);
+            Count--;
+        }
+        public IEnumerable<T> getItems()
+        {
+            return items;
+        }
+        public void printAll()
+        {
+            Console.WriteLine();
+            foreach (var oneitem in getItems())
+            {
+                Console.WriteLine(oneitem.ToString());
+            }
+            Console.WriteLine();
+        }
+        public bool attend(T item)
+        {
+            bool rc = false;
+            foreach (var oneitem in getItems())
+            {
+                if (item.ToString() == oneitem.ToString())
+                    rc = true;
+            }
+            return rc;
+        }
+        public MyCollection<T> Merge(MyCollection<T> second)
+        {
+            MyCollection<T> customer = new MyCollection<T>();
+            foreach (var oneitem in second.getItems())
+            {
+                if (!(this.attend(oneitem)))
+                    customer.addItem(oneitem);
+            }
+            foreach (var oneitem in this.getItems())
+            {
+                if (!(second.attend(oneitem)))
+                    customer.addItem(oneitem);
+            }
+            return customer;
+        }
+    }
+        class Singleton<T> : IDisposable
+        {
+
+            private static Singleton<T> instance;
+
+            private static T[] items = new T[0];
+            public void addItem(T item)
+            {
+                Array.Resize(ref items, items.Count() + 1);
+                items[items.Count() - 1] = item;
+            }
+            public void removeItem(T item)
+            {
+                items[Array.IndexOf(items, item)] = items[items.Count() - 1];
+                Array.Resize(ref items, items.Count() - 1);
+            }
+            public IEnumerable<T> getItems()
+            {
+                return items;
+            }
+            public Singleton()
+            {
+            }
+            public static Singleton<T> getInstance()
+            {
+                if (instance == null)
+                    instance = new Singleton<T>();
+                return instance;
+            }
+            public void printAll()
+            {
+                Console.WriteLine();
+                foreach (var oneitem in getItems())
+                {
+                    Console.WriteLine(oneitem);
+                }
+                Console.WriteLine();
+            }
+            public void Dispose()
+            {
+                GC.SuppressFinalize(this);
+            }
+        }
+    }
+
